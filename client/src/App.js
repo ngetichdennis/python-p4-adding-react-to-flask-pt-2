967b42aedbd768363484debdbd661485f043d4ae
Dennis <ngetichdennis90@gmail.com>
2024-04-11 22:17:21 +0300

    completed assignment

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import {useEffect} from 'react';
+import logo from './logo.svg';
+import './App.css';
+  function App() {
+    useEffect( () => {
+      fetch("/movies")
+        .then((response) => response.json())
+        .then((data) => 
+        console.log(data))
+      },[])
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+      <h1>Check the console for a list of movies!</h1>
+    </div>
+  );
+}
+
+export default App;
